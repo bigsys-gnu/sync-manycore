03e4cf67973c003b96822ae9acfa55f7bbdc2522
MadPlayer <kch9001@gmail.com>
2023-02-02 16:58:52 +0900

    rcu reduce statistics overhead

--- src/datastruct/Concurrent-Skip-list/rcu/statistics.hh
+++ src/datastruct/Concurrent-Skip-list/rcu/statistics.hh
@@ -84,33 +84,48 @@ namespace rcu_stat
   inline std::mutex g_stat_lock;
 
   template <bool ON = true>
-  void count_abort()
+  void count_abort_impl()
   {
     local_stat.abort_counter++;
   }
 
   template <>
-  inline void count_abort<false>() {}
+  inline void count_abort_impl<false>() {}
+
+  inline void count_abort()
+  {
+    count_abort_impl<STAT_ON>();
+  }
 
   template <bool ON = true>
-  void gather_stat()
+  void gather_stat_impl()
   {
     std::unique_lock<std::mutex> lock{g_stat_lock};
     global_stat.abort_counter += local_stat.abort_counter;
   }
 
   template <>
-  inline void gather_stat<false>() {}
+  inline void gather_stat_impl<false>() {}
+
+  inline void gather_stat()
+  {
+    gather_stat_impl<STAT_ON>();
+  }
 
   template <bool ON = true>
-  void print_stat()
+  void print_stat_impl()
   {
     std::cout << "RCU STATISTICS\n"
               << global_stat.abort_counter << '\n';
   }
 
   template <>
-  inline void print_stat<false>() {}
+  inline void print_stat_impl<false>() {}
+
+  inline void print_stat()
+  {
+    print_stat_impl<STAT_ON>();
+  }
 }
 
 #endif /* STATISTICS_H */
